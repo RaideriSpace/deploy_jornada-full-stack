c0cdcb4620ce165f10044d93b7a48a19dd2eac04
Lucas Alves Pinheiro <l.pinheiro.w@gmail.com>
2025-05-12 09:56:36 -0300

    Ajustes para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -7,10 +7,15 @@
 // No MongoDB - Criar conta, criar  Cluster, criar o banco de dadeos, modificar o Network Access
 // Middleware - npm i cors Í Faz um tratamento antes de enviar a resposta.
 
+//npm i path
+
 
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path"
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3050;
@@ -18,16 +23,21 @@ const PORT = 3050;
 app.use(cors());
 // app.use(express.json());
 
-app.get('/', (request, response) => {
+app.get('/api/', (request, response) => {
     response.send("Olá, Mundo!");
 })
-app.get('/artists', async (request, response) => {
+app.get('/api/artists', async (request, response) => {
     response.send(await db.collection("artists").find({}).toArray());
 })
-app.get('/songs', async (request, response) => {
+app.get('/api/songs', async (request, response) => {
     response.send(await db.collection("songs").find({}).toArray());
 })
 
+app.use(express.static(path.join(__dirname, '../front-end/dist')));
+
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, '../front-end/dist/index.html'));
+});
 app.listen(PORT, () => {
     console.log(`Servidor está escutando na porta ${PORT}`);
 })
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,12 @@
 // Fetch (nativo) ou Axios (package)
+
+// npm i dotenv
+import "dotenv/config"
+
 import axios from "axios";
 
-const URL = "http://localhost:3050";
+const {NODE_ENV} = process.env;
+const URL = NODE_ENV === 'development' ? "http://localhost:3050/api" : "/api"; 
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
