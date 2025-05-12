b894d6a344ec57da80952b65a2e013671fcb6a0b
Lucas Alves Pinheiro <l.pinheiro.w@gmail.com>
2025-05-12 10:38:52 -0300

    Ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -5,8 +5,9 @@ import "dotenv/config"
 
 import axios from "axios";
 
-const {NODE_ENV} = process.env;
-const URL = NODE_ENV === 'development' ? "http://localhost:3050/api" : "/api"; 
+// const {NODE_ENV} = process.env;
+
+const URL =  "https://deploy-jornada-full-stack-yckq.onrender.com/api"; 
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
